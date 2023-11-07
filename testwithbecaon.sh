e84e6e5d05eba94f9276f09c498190c7114918c8
ariel <ariel.gabizon@gmail.com>
2023-11-07 17:21:35 +0100

    remove beacon from test

--- testwithbecaon.sh
+++ testwithbecaon.sh
@@ -10,3 +10,9 @@ cargo run --release --bin new_constrained
 cargo run --release --bin compute_constrained
 cargo run --release --bin verify_transform_constrained
 
+mv challenge challenge_old
+mv response response_old
+
+mv new_challenge challenge
+cargo run --release --bin beacon_constrained
+cargo run --release --bin verify_transform_constrained
